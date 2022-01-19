22ec49d1f2987bad60ad673958e0a62a1948c0a7
Francesco <fbattistoni97@gmail.com>
2022-01-19 18:07:13 +0100

    blinking text

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 //   $('.pressed').hide()
 // }, 1000)
 
-
+// function resetHeight(){
+//   // reset the body height to that of the inner browser
+//   document.body.style.height = window.innerHeight + "px";
+// }
+// // reset the height whenever the window's resized
+// window.addEventListener("resize", resetHeight);
+// // called to initially set the height.
+// resetHeight();
 
 // "use strict";
 let minute = 0;
